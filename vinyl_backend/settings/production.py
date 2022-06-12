1aa8f0d4c89e6e6975e39dff313f36901a5a9412
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-12 07:11:18 +0000

    Added STATIC_ROOT setting

--- vinyl_backend/settings/production.py
+++ vinyl_backend/settings/production.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['project-vinyl-backend.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-
+STATIC_ROOT = 'static'
 
 CLOUDINARY_URL = os.environ['CLOUDINARY_URL']
 
