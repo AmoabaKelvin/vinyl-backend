dabe0323921b3c20ad76f4a95ba15a8e2f24b269
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-13 23:20:07 +0000

    Added serializers for user and also added registration serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,7 @@
 from accounts.models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from song.models import Song
 
 
@@ -18,3 +20,72 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'songs')
+
+
+# serializers pertaining to authentication
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registering a new user.
+    """
+
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+    )
+    first_name = serializers.CharField(required=True, min_length=3)
+    last_name = serializers.CharField(required=True, min_length=3)
+    # set password and password2 write_only to True to ensure they are only
+    # used for creating and updating but not to be included when serializing data.
+    password = serializers.CharField(
+        required=True, write_only=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'password2',
+        )
+
+    def validate(self, attrs):
+        """
+        Check whether the two entered passwords match
+        """
+        if attrs.get('password') != attrs.get('password2'):
+            raise serializers.ValidationError(
+                {
+                    "password": "Passwords do not match. Ensure that you have entered the same password twice."
+                }
+            )
+        return attrs
+
+    def create(self, validated_data):
+        """
+        Create and return a new user
+        """
+        user = CustomUser.objects.create(
+            username=validated_data.get('username'),
+            email=validated_data.get('email'),
+            first_name=validated_data.get('first_name'),
+            last_name=validated_data.get('last_name'),
+        )
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serialize instances of the `CustomUser` model.
+    """
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password')
+        # setting the password to write_only ensures that it is not included
+        # in the serialized data
+        extra_kwargs = {'password': {'write_only': True}}
