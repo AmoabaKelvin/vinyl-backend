e2684e08df54f8d3b735db3bccb7f813c0cc8513
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-15 13:10:31 +0000

    Commented out paths leading to CBV's which have been removed

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,11 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('songs/', views.SongList.as_view()),
-    path('songs/<int:id>', views.SongDetail.as_view()),
+    # path('songs/', views.SongList.as_view()),
+    path('songs/', views.song_list_or_create_song),
+    # path('songs/<int:id>', views.SongDetail.as_view()),
+    path('songs/<int:id>', views.retrieve_particular_song),
+    # path('songs/<int:id>', views.retrieve_particular_song),
     path('users/<int:id>', views.UserDetailView.as_view()),
     # redirect all requests to api/accounts to the accounts app
     path('accounts/', include('accounts.urls')),
