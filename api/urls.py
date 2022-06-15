b372707300db37939f7c836ce55725eb2d848614
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-15 13:15:24 +0000

    Commented out UserDetailView and replaced it with a CBV

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # path('songs/<int:id>', views.SongDetail.as_view()),
     path('songs/<int:id>', views.retrieve_particular_song),
     # path('songs/<int:id>', views.retrieve_particular_song),
-    path('users/<int:id>', views.UserDetailView.as_view()),
+    # path('users/<int:id>', views.UserDetailView.as_view()),
+    path('users/<int:id>', views.retrieve_particular_user),
     # redirect all requests to api/accounts to the accounts app
     path('accounts/', include('accounts.urls')),
 ]
