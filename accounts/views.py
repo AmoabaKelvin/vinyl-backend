80c7900ef8919924167759ddbb54ee95f9d857e6
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-15 13:08:04 +0000

    Changed response json structure to include status, details and result attributes

--- accounts/views.py
+++ accounts/views.py
@@ -13,12 +13,29 @@ class SignUpView(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        # serializer.is_valid(raise_exception=True)
+        if not serializer.is_valid():
+            serializer_errors = ''
+            for key, value in serializer.errors.items():
+                # add error to serializer_errors variable
+                serializer_errors += f"{key}: {value[0]} "
+            return Response(
+                data={
+                    'status': 'failure',
+                    'result': '',
+                    'details': serializer_errors,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         user = serializer.save()
         return Response(
             {
-                "user": UserSerializer(user, context=self.get_serializer_context()).data,
-                "token": AuthToken.objects.create(user)[1],
+                'status': 'success',
+                'result': {
+                    'user': UserSerializer(user, context=self.get_serializer_context()).data,
+                    'token': AuthToken.objects.create(user)[1],
+                },
+                'details': '',
             }
         )
 
@@ -29,6 +46,19 @@ class LoginView(KnoxLoginView):
     """
 
     permission_classes = (permissions.AllowAny,)
+
+    def get_post_response_data(self, request, token, instance):
+        # Override the default response data by adding the status and result
+        # objects to the response data.
+        data = super().get_post_response_data(request, token, instance)
+        custom_json = {
+            'status': 'success',
+            'result': {
+                'expiry': data['expiry'],
+                'token': data['token'],
+            },
+        }
+        return custom_json
 
     def post(self, request):
         serializer = AuthTokenSerializer(data=request.data)
@@ -43,4 +73,4 @@ class LoginView(KnoxLoginView):
 class CustomLogoutView(LogoutView):
     def post(self, request, format=None):
         super(CustomLogoutView, self).post(request, format)
-        return Response({"logged_out": True}, status=status.HTTP_200_OK)
+        return Response({'status': 'success'}, status=status.HTTP_200_OK)
