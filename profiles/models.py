643edc2f35b37a9a5fdd0b7143e80aa847fbce03
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-16 09:29:46 +0000

    Added ArtistCustomerProfile and NormalCustomerProfile

---
+++ profiles/models.py
@@ -0,0 +1,12 @@
+from accounts.models import CustomUser
+from django.db import models
+
+
+class ArtistCustomerProfile(models.Model):
+    artist = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='artist_profile')
+    artistid = models.CharField(max_length=100)
+
+
+class NormalCustomerProfile(models.Model):
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='normal_profile')
+    customerid = models.CharField(max_length=100)
