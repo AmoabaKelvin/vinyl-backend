20d8e16da8e84c9be72a2761a4e47c985ce0c489
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-16 17:37:49 +0000

    Added function to create stripe account

---
+++ accounts/utils.py
@@ -0,0 +1,25 @@
+import os
+
+import stripe
+from profiles.models import ArtistCustomerProfile, NormalCustomerProfile
+
+
+def create_stripe_customer(instance, stripe_data: dict):
+    stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+    customer = stripe.Customer.create(
+        email=instance.email,
+        description=instance.username,
+    )
+    NormalCustomerProfile.objects.create(customer=instance, customerid=customer.id)
+    if instance.is_artist:
+        # create a stripe account for the artist
+        # this account will be used for receiving payments in the future
+        artist_account = stripe.Account.create(
+            type="express",
+            country="US",
+            email=instance.email,
+            business_type="individual",
+            individual=stripe_data,
+        )
+        # create a customer profile for the artist
+        ArtistCustomerProfile.objects.create(artist=instance, artistid=artist_account.id)
