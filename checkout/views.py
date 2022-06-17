d5af61750de6648e1da9f2aed533e9da5aae29ec
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-17 00:47:59 +0000

    Added view to retrieve account balance

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,7 @@ from api.serializers import PaymentInfoSerializer
 from api.utils import return_structured_data
 from django.shortcuts import get_object_or_404
 from dotenv import load_dotenv
+from profiles.models import ArtistCustomerProfile
 
 # from profiles.models import ArtistCustomerProfile
 from rest_framework import permissions, status
@@ -13,6 +14,8 @@ from rest_framework.response import Response
 from song.models import Song
 
 load_dotenv()
+
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
 
 @api_view(['POST'])
@@ -27,7 +30,6 @@ def buy_song(request, song_id):
     # get payment data from serializer
     payment_serializer_data = PaymentInfoSerializer(data=request.data)
     if payment_serializer_data.is_valid():
-        stripe.api_key = os.environ['STRIPE_SECRET_KEY']
         amount = payment_serializer_data.validated_data['amount']
         currency = payment_serializer_data.validated_data['currency']
         payment_intent = stripe.PaymentIntent.create(
@@ -39,3 +41,28 @@ def buy_song(request, song_id):
             return_structured_data('success', payment_intent.client_secret, ''),
             status=status.HTTP_200_OK,
         )
+
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def retrieve_account_balance(request):
+    """
+    Retrieve the account balance of an artist
+    """
+    # get the artist customer profile
+    artist_customer_profile = ArtistCustomerProfile.objects.get(artist=request.user)
+    # get the account balance
+    try:
+        response: dict = stripe.Balance.retrieve(
+            stripe_account=artist_customer_profile.artistid
+        )
+    except Exception as e:
+        response = {'error': 'error retrieving account balance'}
+        return Response(return_structured_data('failure', response, ''))
+    available_balance = response['available'][0]['amount']
+    pending_balance = response['pending'][0]['amount']
+    response_data = {
+        'available_balance': available_balance,
+        'pending_balance': pending_balance,
+    }
+    return Response(return_structured_data('success', response_data, ''))
