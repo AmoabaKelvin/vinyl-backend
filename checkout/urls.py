13faa40829393b3bc492c105b3dc7368c154533c
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-17 14:04:19 +0000

    Update account info endpoint

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('buy/<int:song_id>', views.buy_song, name='buy_song'),
     path('balance', views.retrieve_account_balance, name='balance'),
     path('account/info', views.retrieve_account_info, name='account'),
-    # path('account/update', views.update_account_info, name='account_update'),
+    path('account/update', views.update_account_info, name='account_update'),
 ]
