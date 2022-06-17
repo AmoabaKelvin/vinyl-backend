e6fa4f6bb87150a60df5cc5b0b8a4252e241079f
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-17 00:48:33 +0000

    Added search url

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('songs/', views.song_list_or_create_song),
     path('songs/<int:id>', views.retrieve_particular_song),
     path('users/<int:id>', views.retrieve_particular_user),
+    path('songs/search/<str:song_name>', views.search_song),
     path('accounts/', include('accounts.urls')),
     # redirect all payment request to the checkout app
     path('checkout/', include('checkout.urls')),
