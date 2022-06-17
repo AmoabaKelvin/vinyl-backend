905f77733b7711f278ee749b4face6b97cf2010c
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-17 11:15:43 +0000

    Added url to account info

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('buy/<int:song_id>', views.buy_song, name='buy_song'),
     path('balance', views.retrieve_account_balance, name='balance'),
+    path('account/info', views.retrieve_account_info, name='account'),
+    # path('account/update', views.update_account_info, name='account_update'),
 ]
