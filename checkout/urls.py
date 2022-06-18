82e10bf87f6dfd59aabbc793e258eb989075c510
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-18 15:37:27 +0000

    Added request payout url

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('balance', views.retrieve_account_balance, name='balance'),
     path('account/info', views.retrieve_account_info, name='account'),
     path('account/update', views.update_account_info, name='account_update'),
+    path('payout', views.request_payout, name='payout'),
     # path('account/delete', views.delete_account, name='account_delete'),
     path('done', views.display_thank_you, name='done'),
 ]
