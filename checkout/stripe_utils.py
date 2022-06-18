4a8f476e6120b575665b9d3186034fd6ba09cd00
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-18 15:36:16 +0000

    Added extra functions to interact with stripe API

--- checkout/stripe_utils.py
+++ checkout/stripe_utils.py
@@ -2,8 +2,6 @@
 from typing import Tuple
 
 import stripe
-from api.utils import return_structured_data
-from rest_framework.response import Response
 
 
 def retrive_connect_account_balance(account_id: str) -> Tuple[str, str]:
@@ -11,10 +9,9 @@ def retrive_connect_account_balance(account_id: str) -> Tuple[str, str]:
     Retrieve account balance for a user(artist).
     """
     # https://stripe.com/docs/api/balance/balance_object
-    response: dict = stripe.Balance.retrieve(stripe_account='acct_1LByrHQjPLYfvCoi')
+    response: dict = stripe.Balance.retrieve(stripe_account=account_id)
     available_balance = response['available'][0]['amount']
     pending_balance = response['pending'][0]['amount']
-    print(available_balance, pending_balance)
     return available_balance, pending_balance
 
 
@@ -23,22 +20,20 @@ def retrive_bank_account_info(account_id: str) -> str:
     Retrieve bank account information for a user(artist).
     """
     # https://stripe.com/docs/api/external_account_bank_accounts/retrieve
-
-    # for testing 'acct_1LByrHQjPLYfvCoi'
-    response: dict = stripe.Account.retrieve('acct_1LByrHQjPLYfvCoi')
+    response: dict = stripe.Account.retrieve(account_id)
     bank_id: str = response['external_accounts']['data'][0]['id']
     return bank_id
 
 
-def initiate_payout_request(bank_id: str, amount: int, currency: str = 'usd') -> dict:
+def initiate_payout_request(user_account: str, amount: int, currency: str = 'usd'):
     """
     Initiate a payout request for a user(artist).
     """
     # https://stripe.com/docs/api/payouts/create
     response: dict = stripe.Payout.create(
-        amount=amount,
+        amount=int(amount / 10),
         currency=currency,
-        destination=bank_id,
+        stripe_account=user_account,
     )
     return response
 
@@ -52,3 +47,54 @@ def create_ephemeral_key(customer_id: str) -> str:
         customer=customer_id, stripe_version='2020-08-27'
     )
     return response.secret
+
+
+def update_account_information(account_id: str, update_data: dict) -> dict:
+    """
+    Update account information for a user(artist).
+    """
+    # https://stripe.com/docs/api/accounts/update
+    response: dict = stripe.Account.modify(
+        account_id,
+        business_type='individual',
+        metadata=update_data,
+    )
+    return response
+
+
+def create_payment_intent(amount: int, destination: str, customer: str):
+    """
+    Create a payment intent and return the client secret
+    Args:
+        amount: amount to be paid
+        destination: stripe account id of the recipient
+        customer: stripe account id of the customer
+    Returns:
+        client_secret: client secret of the payment intent
+    https://stripe.com/docs/api/payment_intents/create
+    """
+    price: int = int(amount * 100)
+    response: dict = stripe.PaymentIntent.create(
+        amount=price,
+        currency='usd',
+        application_fee_amount=int(price * 0.03),
+        transfer_data={
+            'destination': destination,
+        },
+        automatic_payment_methods={'enabled': True},
+        customer=customer,
+    )
+    return response.client_secret
+
+
+def retrieve_account_information(account_id: str) -> dict:
+    """
+    Retrieve account information for a user.
+    Args:
+        account_id: stripe account id of the user
+    Returns:
+        response: dictionary of account information
+    https://stripe.com/docs/api/accounts/retrieve
+    """
+    response: dict = stripe.Account.retrieve(account_id)
+    return response
