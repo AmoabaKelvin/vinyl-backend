85d19dd4146b8d941e7aa36bcb3085db8b1be8a3
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-30 11:40:54 +0000

    Replaced double quotes with single quotes to maintain consistency

--- song/admin.py
+++ song/admin.py
@@ -7,35 +7,35 @@ from .models import Song
 
 class SongAdmin(admin.ModelAdmin):
     list_display: Sequence[str] = [
-        "artist",
-        "title",
-        "genre",
-        "producer",
-        "release_date",
-        "created_at",
-        "updated_at",
+        'artist',
+        'title',
+        'genre',
+        'producer',
+        'release_date',
+        'created_at',
+        'updated_at',
     ]
     list_filter: Sequence[str] = [
-        "artist",
-        "genre",
-        "producer",
-        "writer",
-        "release_date",
-        "created_at",
-        "updated_at",
+        'artist',
+        'genre',
+        'producer',
+        'writer',
+        'release_date',
+        'created_at',
+        'updated_at',
     ]
     search_fields: Sequence[str] = [
-        "artist",
-        "title",
-        "genre",
-        "producer",
-        "featured_artists",
-        "writer",
-        "release_date",
-        "created_at",
-        "updated_at",
+        'artist',
+        'title',
+        'genre',
+        'producer',
+        'featured_artists',
+        'writer',
+        'release_date',
+        'created_at',
+        'updated_at',
     ]
-    readonly_fields: Sequence[str] = ["created_at", "updated_at"]
+    readonly_fields: Sequence[str] = ['created_at', 'updated_at']
 
 
 admin.site.register(Song, SongAdmin)
