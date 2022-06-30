1b07a0f8fffb69933504c2f60e623202c9e212bd
Kelvin Amoaba <kel.amoaba@gmail.com>
2022-06-30 11:39:51 +0000

    Replaced all double quotes with single quotes to maintain consistency

--- song/models.py
+++ song/models.py
@@ -7,15 +7,17 @@ class Song(models.Model):
     Model for a song
     """
 
-    artist = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, related_name="songs")
+    artist = models.ForeignKey(
+        to=CustomUser, on_delete=models.CASCADE, related_name='songs'
+    )
     title = models.CharField(max_length=255)
     genre = models.CharField(max_length=100)
     producer = models.CharField(max_length=150)
     featured_artists = models.CharField(max_length=255, blank=True)
     writer = models.CharField(max_length=150, blank=True)
     release_date = models.DateField()
-    cover_art = models.ImageField(upload_to="cover_arts/", blank=True)
-    audio_file = models.FileField(upload_to="audio_files/")
+    cover_art = models.ImageField(upload_to='cover_arts/', blank=True)
+    audio_file = models.FileField(upload_to='audio_files/')
     # the track duration will be calculated based on the audio file
     track_duration = models.CharField(max_length=8, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
